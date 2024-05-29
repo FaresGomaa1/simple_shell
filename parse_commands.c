f4578e6889fb2367c985c4fe9ff12b4be4ad109c
FaresGomaa1 <faresgomaa336@gmail.com>
2024-05-29 19:36:15 +0300

    done

---
+++ parse_commands.c
@@ -0,0 +1,27 @@
+#include "header_file.h"
+
+/**
+* main - Entry point of the program
+* @argc: Number of arguments passed to the program
+* @argv: Array of arguments passed to the program
+* Return: 0 on success, -1 on failure
+*/
+int main(int argc, char **argv)
+{
+/* Your code here */
+
+return (0);
+}
+
+/**
+* function_name - Brief description of the function
+* @param1: Description of parameter 1
+* @param2: Description of parameter 2
+* Return: Description of return value
+*/
+int function_name(int param1, char *param2)
+{
+/* Your function implementation here */
+
+return (0);
+}
